d954d8fcebdf4a73ed2926b34e004a79c4779a4f
MICKBOUT <matisse.boutte@gmail.com>
2025-12-21 18:27:32 +0100

    tree module 00

---
+++ module_00/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,10 @@
+def ft_count_harvest_recursive(current_day: None = None, end: None = None) -> None:
+    if end is None:
+        end = int(input("Days until harvest: "))
+        ft_count_harvest_recursive(1, end)
+    else:
+        if current_day <= end:
+            print(f"Day {current_day}")
+            ft_count_harvest_recursive(current_day + 1, end)
+        else:
+            print("Harvest time!")
