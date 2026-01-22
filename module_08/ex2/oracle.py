36a991bbb803537459d8cb6dabd9bd23e84eb03c
MICKBOUT <matisse.boutte@gmail.com>
2026-01-22 19:07:16 +0100

    strat of ex

---
+++ module_08/ex2/oracle.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+
+
+env_loaded = load_dotenv()
+
+
+def configuration() -> None:
+    api_keys = {"secret123",  "mboutte_key"}
+
+    matrix_mode = os.getenv("MATRIX_MODE")
+    database_url = os.getenv("DATABASE_URL")
+    api_key = os.getenv("API_KEY")
+    log_level = os.getenv("LOG_LEVEL")
+    zion_endpoint = os.getenv("ZION_ENDPOINT")
+
+    print("Configuration loaded:")
+    print("Mode:", matrix_mode)
+    print("Database:",
+          "Connected to local instance"
+          if "http://localhost/" == database_url[:17]
+          else "Not found")
+    print("API Access:",
+          "Authenticated" if api_key in api_keys else "Unverified")
+    print(log_level)
+    print(zion_endpoint)
+
+
+def missing_configuration_warnings() -> None:
+    print("\nWarnings: Configue file missing")
+    print("Setup a configue file to configure the environement\n")
+
+
+if __name__ == "__main__":
+    env_loaded = load_dotenv()
+    if not env_loaded:
+        missing_configuration_warnings()
+    else:
+        print("\nORACLE STATUS: Reading the Matrix...\n")
+        configuration()
