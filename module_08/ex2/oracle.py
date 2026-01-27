91618622001ca5343d7bf6d382025c90cc59f29e
MICKBOUT <matisse.boutte@gmail.com>
2026-01-27 10:49:59 +0100

    fix apikey hardcoded

--- module_08/ex2/oracle.py
+++ module_08/ex2/oracle.py
@@ -1,30 +1,26 @@
 from dotenv import load_dotenv
-import hashlib
 import os
 
 
-def hash_message(message: str) -> str:
-    # hash the str in parameter
-    return hashlib.sha256(message.encode("utf-8")).hexdigest()
+def looks_like_api_key(key: str) -> bool:
+    return len(key) >= 6 and key[-3:].isnumeric() and key[:3].isalpha()
 
 
 def configuration(env_arg: list) -> None:
-    api_keys = {
-        "fcf730b6d95236ecd3c9fc2d92d7b6b2bb061514961aec041d6c7a7192f592e4",
-        "5c58b8f7c6d1b58b5d1bd5f7e25f186c0c8f58b17d8d3dfbac2b6f618d7835e6"
-        }
 
     # unpack the arg
     matrix_mode, database_url, api_key, log_level, zion_endpoint = env_arg
 
+    # print the env info
     print("Configuration loaded:")
     print("Mode:", matrix_mode)
     print("Database:",
-          "Connected to local instance" if database_url[:17] == "http://localhost/"
+          "Connected to local instance"
+          if database_url[:17] == "http://localhost/"
           else "Not found")
     print("API Access:",
-          "Authenticated" if hash_message(api_key) in api_keys else "Unverified")
-    print("log_level:", log_level)
+          "Authenticated" if looks_like_api_key(api_key) else "Unverified")
+    print("log level:", log_level)
     print("zion Network:", zion_endpoint)
     print()
 
